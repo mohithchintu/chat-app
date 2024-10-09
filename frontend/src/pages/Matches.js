fc8a0497a92c043cde3579a5dff5343213213b48
mohithchintu <93631804+mohithchintu@users.noreply.github.com>
2024-10-09 19:06:56 +0530

    Update Matches.js

--- frontend/src/pages/Matches.js
+++ frontend/src/pages/Matches.js
@@ -12,7 +12,7 @@ const Matches = () => {
     const fetchMatches = async () => {
       try {
         const response = await axios.get(
-          `http://localhost:5000/api/matches/${user._id}`
+          `http://localhost:5002/api/matches/${user._id}`
         );
         setMatches(response.data);
 
